26e3e855d5ef9323b4cb55916ef44304a00092bf
emillyalmeida <emillyalmeidan@gmail.com>
2022-02-03 22:22:30 -0300

    butao reset funcionando

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ buttonIniciar.innerText = "Iniciar"
 buttonIniciar.id = "iniciar"
 const buttonReset = document.createElement('button')
 buttonReset.innerText = "Reset"
+buttonReset.id = "reset"
 
 header.appendChild(h1)
 header.appendChild(buttonIniciar)
@@ -17,43 +18,49 @@ header.appendChild(buttonReset)
 containerTotal.appendChild(header)
 
 
+const main = document.createElement('main')
+main.id ="main"
+
+containerTotal.appendChild(main)
+
 const start = document.getElementById('iniciar')
+const resetar = document.getElementById("reset")
+
+let count = false
 
 function createElements(){
-    for(let i =1; i<4;i++){
-        const divContainer = document.createElement('div')
-        divContainer.id = "container-torre" + i
-        divContainer.classList.add("container_total")
-
-        const divVareta = document.createElement('div')
-        divVareta.classList.add("vareta")
-        const divDiscos = document.createElement('ul')
-
-        divContainer.appendChild(divDiscos)
-        divContainer.appendChild(divVareta)
-        containerTotal.appendChild(divContainer)
-    }
-    function createDiscos(){
-        const torre1 = document.getElementById("container-torre1")
-        const ul = torre1.getElementsByTagName("ul")
-        
-        const liMaior = document.createElement('li')
-        liMaior.classList.add('maior')
-        const liMedia1 = document.createElement('li')
-        liMedia1.classList.add('media1')
-        const liMedia2 = document.createElement("li")
-        liMedia2.classList.add('media2')
-        const liMenor = document.createElement("li")
-        liMenor.classList.add('menor')
+    if(count==false){
+        for(let i =1; i<4;i++){
+            const divDiscos = document.createElement('ul')
+            divDiscos.id = "container-torre" + i
+            divDiscos.classList.add("container_total")
     
-        ul[0].appendChild(liMaior)
-        ul[0].appendChild(liMedia1)
-        ul[0].appendChild(liMedia2)
-        ul[0].appendChild(liMenor)
-
+            main.appendChild(divDiscos)
+        }
+        createDiscos()
     }
-    createDiscos()
 }
+
+function createDiscos(){
+    const torre1 = document.getElementById("container-torre1")
+
+    const liMaior = document.createElement('li')
+    liMaior.classList.add('maior')
+    const liMedia1 = document.createElement('li')
+    liMedia1.classList.add('media1')
+    const liMedia2 = document.createElement("li")
+    liMedia2.classList.add('media2')
+    const liMenor = document.createElement("li")
+    liMenor.classList.add('menor')
+
+    torre1.appendChild(liMaior)
+    torre1.appendChild(liMedia1)
+    torre1.appendChild(liMedia2)
+    torre1.appendChild(liMenor)
+
+    count = true
+}
+
 
 start.addEventListener('click',createElements)
 
@@ -72,8 +79,8 @@ function selecionarDisco(event) {
             countclick += 1
         }else if(countclick==1){
             checkSizeLi(torre)
+            isWinner()
         }
-          
     }
 }
 
@@ -84,16 +91,70 @@ function checkSizeLi (torre){
         let sizePutDisc = lastDisco.offsetWidth
         if(sizeDisc>sizePutDisc){
             torre.appendChild(lastDisco)
-            countclick=0
+            countclick = 0
 
         }else{
             alert('Os dicos maiores não podem ser em cima dos menores')
-            countclick=0
+            countclick = 0
         }
     }else{
         torre.appendChild(lastDisco)
-        countclick=0
+        countclick = 0
     }
 }
 
-containerTotal.addEventListener('click',selecionarDisco, event)
+containerTotal.addEventListener('click',selecionarDisco)
+
+function isWinner(){
+    // const torre1 = document.getElementById('container-torre1')
+    // const torre2 = document.getElementById('container-torre2')
+    // const torre3 = document.getElementById('container-torre3')
+    if(main.childNodes[1].childElementCount == 4|| main.childNodes[2].childElementCount == 4){
+        removeAll()
+        winnerMensage()
+    }
+ }
+
+ function winnerMensage (){
+    const section = document.createElement('section')
+    section.classList.add('winner')
+    const img = document.createElement('img')
+    img.src = 'css/winner.gif'
+    const h2 = document.createElement('h2')
+    h2.innerText = "Parabéns você ganhou !!! Click em reset para jogar de novo"
+
+    section.appendChild(img)
+    section.appendChild(h2)
+
+    main.appendChild(section)
+     
+ }
+
+
+
+function removeAll(){
+    let remover = main.childNodes
+    
+    for(let i = 0;i<remover.length;i++){
+         remover[i].remove()
+      }
+    // let remove = remover.forEach((elemt)=>elemt.remove())
+    // console.log(remove)
+    if(remover.length>0){
+        let removeAgain = remover[0].remove()
+        return removeAgain
+    }else{
+        return remover
+    }
+    
+    
+}
+
+
+function resetAll(){
+    removeAll()
+    count = false
+    createElements()
+}
+
+resetar.addEventListener('click',resetAll)
